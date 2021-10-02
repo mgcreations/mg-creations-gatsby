2dffddeface7419e3da8edd3f1ac45b280c8be59
mgcreations <info@mgcreations.co.za>
2021-10-02 07:24:35 +0200

    Adding layout & content to Services Page

---
+++ src/pages/services.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
+
+const Services = (props) => (
+  <Layout>
+    <Helmet>
+      <title>MG Creations | Services</title>
+      <meta name="description" content="Services offered by MG Creations" />
+    </Helmet>
+  </Layout>
+);
+
+export default Services;
