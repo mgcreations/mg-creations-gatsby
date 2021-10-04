7a0320547d1a447500634784f27d52f225b3f5fe
mgcreations <info@mgcreations.co.za>
2021-10-04 05:06:29 +0200

    made vidoe responsive

--- src/pages/services.js
+++ src/pages/services.js
@@ -140,7 +140,7 @@ const Services = (props) => (
         </section>
         <section>
           <span className="image">
-            <video controls width="576">
+            <video controls>
               <source src={vidLogo} type="video/mp4" />
             </video>
           </span>
